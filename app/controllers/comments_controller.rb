69957bb8a91e796e336240170f9bb444582f92e4
root <root@pierr-desktop.(none)>
2010-10-24 20:57:48 +0800

    1.be able to edit user 2.formatter the view of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,11 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
 
+    #if user has already logined int, set the user
+    if current_user
+      @comment.user_id = current_user.id
+    end
+
     redirect_to post_path(@post)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,23 +41,39 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
 
-    respond_to do |format|
+    #respond_to do |format|
       if @user.save
         #session[:user] = User.authenticate(@user.login, @user.password)
         flash[:notice] = "Registration successful"
-        #redirect_to root_url
-        format.html { redirect_to(@user, :notice => 'You have successfully registed.') }
+        redirect_to root_url
+        #format.html { redirect_to(@user, :notice => 'You have successfully registed.') }
         #format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        render :action => "new"
+        #format.html { render :action => "new" }
+        #format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    #end
+  end
+
+ 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to(@user, :notice => 'Profile was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @post.errors, :status => :unprocessable_entity }
       end
     end
   end
-
-  
-
-
 
  
 
