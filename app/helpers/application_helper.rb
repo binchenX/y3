0d3a71de07a1e8e2859cea157ecca3213eac4363
root <root@pierr-desktop.(none)>
2010-10-29 17:47:41 +0800

    markdown /wmd works - except that we have to escape the html content converted by the wmd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+   def javascripts
+    javascripts = ['jquery', 'rails', 'jquery.autocomplete', 'prettify', 'wmd', 'application']
+#    if Rails.env == 'production'
+#      javascripts << 'google_analytics'
+#    end
+#    if Rails.env == 'development'
+#      javascripts << ['showdown', 'wmd-base', 'wmd-plus']
+#    end
+    javascripts << 'uservoice'
+  end
+  
 end
