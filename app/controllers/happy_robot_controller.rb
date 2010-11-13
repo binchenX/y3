7c0b9affc17ac4dd88ba87194ad3c5bec1fe50d2
pierrchen <pierr.chen@gmail.com>
2010-11-13 11:27:24 +0800

    change find post with 简介 only

--- app/controllers/happy_robot_controller.rb
+++ app/controllers/happy_robot_controller.rb
@@ -3,15 +3,42 @@ require 'open-uri'
 require 'nokogiri'
 require 'pp'
 
+#this is the index page 
+#@uri = "http://bbs.sjtu.edu.cn/bbstdoc,board,PopMusic.html"
 
-@uri = "http://bbs.sjtu.edu.cn/bbstdoc,board,PopMusic.html"
+@uri = "http://bbs.sjtu.edu.cn/bbstdoc,board,PopMusic,Page,175.html"
 
 #Alougth the site declared to be GB2312 ,but same character are actually not, which
 #will result in truncated document. Declaring it as GB18030 will solve this problem
 doc = Nokogiri::HTML(open(@uri), nil, "GB18030") 
-doc.xpath('//a').each do |node|
 
-  p node['href']	
-  p node.text
+#select only the post links
 
+def isPostLink? href
+	href.include?"reid"
 end
+
+def isAlbumIntr? title
+	#contains [\347\256\200\344\273\213] - UTF-8
+	title.include?"[简介]"
+end
+
+#the artist is right after [jian jie]
+def isInterestedAuthor? title
+	true
+end
+
+posts_in_current_page = doc.xpath('//a').select do |node|
+	isPostLink? node['href'] and 
+	isAlbumIntr? node.text and
+	isInterestedAuthor? node.text 
+end
+
+posts_in_current_page.each do |node|
+
+   link = node['href']	
+   title = node.text
+
+   puts title
+   puts link 
+end 
