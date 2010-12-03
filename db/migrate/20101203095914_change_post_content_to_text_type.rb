ce038fc44e60c340b5a27d1b8036a242cff76c9a
pierrchen <pierr.chen@gmail.com>
2010-12-03 18:06:35 +0800

    migrate posts:content to text type

---
+++ db/migrate/20101203095914_change_post_content_to_text_type.rb
@@ -0,0 +1,10 @@
+class ChangePostContentToTextType < ActiveRecord::Migration
+  def self.up
+    change_column :posts, :content, :text
+    
+  end
+
+  def self.down
+    change_column :posts, :content, :string
+  end
+end
