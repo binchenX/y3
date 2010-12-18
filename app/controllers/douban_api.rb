40f42d51f6f814b50193db2064dd76f76aadafb5
pierrchen <pierr.chen@gmail.com>
2010-12-18 19:54:26 +0800

    parse starttime , location ok

--- app/controllers/douban_api.rb
+++ app/controllers/douban_api.rb
@@ -28,7 +28,7 @@ doc.xpath("//entry").each do |entry|
   title = entry.at_xpath(".//title").text
   link =  entry.at_xpath(".//link[@rel='alternate']")["href"]
   #attribute is starttime NOT startTime as specified in the xml
-  start = entry.at_xpath('.//when')["starttime"]
+  start = entry.at_xpath('.//gd:when')["starttime"]
   location = entry.at_xpath('.//location').text
   puts title
   puts link
