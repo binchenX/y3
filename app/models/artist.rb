ddd7d5aba6753fa230494dcd936afdecf53cb4e3
pierrchen <pierr.chen@gmail.com>
2010-12-18 22:09:35 +0800

    use markdown for artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ActiveRecord::Base
+  before_save :generate_html_intro
+
+  
+  def generate_html_intro
+    #this function will called when update
+    self.html_intro = RDiscount.new(intro).to_html
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101218120248) do
+ActiveRecord::Schema.define(:version => 20101218140332) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
     t.text     "intro"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "html_intro"
   end
 
   create_table "comments", :force => true do |t|
