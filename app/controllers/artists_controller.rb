428ce71bbaff7042965d3a2fdb1016819f6aacf9
pierrchen <pierr.chen@gmail.com>
2011-02-10 21:17:50 +0800

    PG report error:  column artists.id must appear in the GROUP BY clause or be used in an aggregate function

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < InheritedResources::Base
 
 	protected
 		def collection
-			@artists = Artist.find(:all,:group => "name" ,:order => "name")
+			#@artists = Artist.find(:all,:group => "name" ,:order => "name")
+			@artists = Artist.find(:all,:order => "name")
 		end
 end
