8b9a455c56ebb8964801c0b3b36b66646fcc29ef
Pierr Chen <pierr.chen@gmail.com>
2012-07-28 13:41:28 +0800

    use development config

--- config/configatron/production.rb
+++ config/configatron/production.rb
@@ -2,3 +2,8 @@
 # 
 # Example:
 #   configatron.file.storage = :s3
+
+configatron.apn.passphrase = ''
+configatron.apn.port = 2195
+configatron.apn.host = 'gateway.sandbox.push.apple.com'
+configatron.apn.cert = File.join(RAILS_ROOT, 'config', 'apple_push_notification_development.pem')
